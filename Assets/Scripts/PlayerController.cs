b85dc54b8a1b1c7845cef502cbec9b2b1241dea5
NicholasVoss <envy_456@yahoo.ca>
2020-09-29 15:40:19 -0400

    Chapter 5 - added enemy AI to follow player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -20,6 +20,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //move player cahracter
         Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"),
             0, Input.GetAxis("Vertical"));
         characterController.SimpleMove(moveDirection * moveSpeed);
